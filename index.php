d458d63481ad4fb334ceb1b7c9037c812fc79b9d
alquizaerwin <47465256+alquizaerwin@users.noreply.github.com>
2019-02-22 16:31:10 +0800

    Delete index.php

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php include_once "header.php"; ?>
-<body class="app header-fixed sidebar-fixed aside-menu-fixed sidebar-lg-show shadow-lg">
-   <header class="app-header navbar">
-      <button class="navbar-toggler sidebar-toggler d-lg-none mr-auto" type="button" data-toggle="sidebar-show">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-   </header>
-<?php include_once "footer.php"; ?>
